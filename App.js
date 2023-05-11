20a6b2d85f4c048a312a48614901284c2e5960d9
Raphael Busquet <raphaelbusquet@outlook.com>
2023-05-11 09:21:38 +0100

    progressing

--- App.js
+++ App.js
@@ -15,7 +15,14 @@ export default class App extends Component {
         <Text style={styles.instructions}>Tamanho da Grade:
           {params.getRowsAmount()}x{params.getColumnsAmount()}
         </Text>
+        
         <Field />
+        <Field opened />
+        <Field opened nearMines={1} />
+        <Field opened nearMines={2} />
+        <Field opened nearMines={3} />
+        <Field opened nearMines={6} />
+
         <StatusBar style="auto" />
       </View>
     );
