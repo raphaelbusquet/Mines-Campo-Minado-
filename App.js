106d5d8b50fe5f010a80c9f66ca81d79cb7f739b
Raphael Busquet <raphaelbusquet@outlook.com>
2023-05-11 10:35:23 +0100

    progressing logic

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-import Field from './src/components/Field'
+import Field from './src/components/Field';
 
 // Parameters
 import params from './src/params';
@@ -15,14 +15,18 @@ export default class App extends Component {
         <Text style={styles.instructions}>Tamanho da Grade:
           {params.getRowsAmount()}x{params.getColumnsAmount()}
         </Text>
-        
+
         <Field />
         <Field opened />
         <Field opened nearMines={1} />
         <Field opened nearMines={2} />
         <Field opened nearMines={3} />
         <Field opened nearMines={6} />
-
+        <Field mined />
+        <Field mined opened />
+        <Field mined opened exploded />
+        <Field flagged />
+        <Field flagged opened />
         <StatusBar style="auto" />
       </View>
     );
--- src/components/Field.js
+++ src/components/Field.js
@@ -2,12 +2,17 @@ import React from "react";
 import { View, StyleSheet, Text } from "react-native";
 import params from '../params'
 
+import Mine from './Mine'
+import Flag from "./Flag";
+
 export default props => {
-    const { mined, opened, nearMines } = props
+    const { mined, opened, nearMines, exploded, flagged } = props
 
     const styleField = [styles.field]
     if (opened) styleField.push(styles.opened)
-    if (styleField.length === 1) styleField.push(styles.regular)
+    if (exploded) styleField.push(styles.exploded)
+    if (flagged) styleField.push(styles.flagged)
+    if (!opened && !exploded) styleField.push(styles.regular)
 
     let color = null
     if (nearMines > 0){
@@ -24,6 +29,8 @@ export default props => {
                     {nearMines}
                 </Text> :
                 false}
+            {mined && opened ? <Mine /> : false}
+            {flagged && !opened ? <Flag /> : false}
         </View>
     )
 }
@@ -50,5 +57,9 @@ const styles = StyleSheet.create({
     label: {
         fontWeight: 'bold',
         fontSize: params.fontSize,
+    },
+    exploded: {
+        backgroundColor: 'red',
+        borderColor: 'red',
     }
 })
